ded063a555e072f7ed7e4362d14f8fd535912cbd
Sahib <sahibmammadov122@gmail.com>
2025-12-21 01:41:03 +0400

    I connected to the mysql and added models

--- littlelemon/restaurant/models.py
+++ littlelemon/restaurant/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Menu(models.Model):
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    inventory = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return self.title
+
+class Booking(models.Model):
+    name = models.CharField(max_length=255)
+    no_of_guests = models.PositiveIntegerField()
+    date = models.DateField()
+
+    def __str__(self):
+        return f"{self.name} ({self.date})"

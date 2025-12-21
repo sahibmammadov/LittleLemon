5a45c43eb48ec8843c983df1e184654fbf6a72f3
Sahib <sahibmammadov122@gmail.com>
2025-12-21 18:07:49 +0400

    created some REST API views using 2 methods one with viewset another is just createapiview

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,5 +1,22 @@
 from django.shortcuts import render
-
+from .models import Booking, Menu
+from .serializers import MenuSerializer, BookingSerializer
+from rest_framework import generics, viewsets, permissions
 # Create your views here.
+
 def index(request):
     return render(request, 'indexs.html')
+
+class MenuItemsView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class SingleItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
